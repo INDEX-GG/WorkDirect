151e796c93c6d31b5268b8d512d74ee48aee8660
sergeyM <cerhlhgr2@gmail.com>
2021-10-06 15:54:31 +0500

    change

--- project-root/app/Views/home.php
+++ project-root/app/Views/home.php
@@ -256,6 +256,18 @@
     <script src="/docs/5.0/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8"
         crossorigin="anonymous"></script>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6988243691587850"
+        crossorigin="anonymous"></script>
+    <!-- WorkDirect -->
+    <ins class="adsbygoogle"
+        style="display:block"
+        data-ad-client="ca-pub-6988243691587850"
+        data-ad-slot="8062000531"
+        data-ad-format="auto"
+        data-full-width-responsive="true"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
 </body>
 
 </html>
